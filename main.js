b0e8f4ed1a805cf8c23b8228a69971d139fa2ae8
Jenny Juyoung Jung <84524297+jennyjjung@users.noreply.github.com>
2021-11-02 11:41:06 -0400

    JS: Home Contact

--- main.js
+++ main.js
@@ -26,7 +26,22 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return; 
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: 'smooth'});
+  
+  //console.log(event.target.dataset.link);
+  scrollIntoView(link);
 });
+
+
+// Hadle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+
+homeContactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+
+// extract method that is repeating
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
