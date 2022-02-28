d51884b4c69ec7fa7b6eb01f062e534b2d3596a4
Jenny Juyoung Jung <jungjuyo@sheridancollege.ca>
2022-02-28 11:11:20 -0500

    JS remove active from Nav

--- .history/main_20220228110446.js
+++ .history/main_20220228110446.js
@@ -74,11 +74,8 @@ workBtnContainer.addEventListener('click', (e) => {
   }
 
   // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target = 
-    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+  const active = document.querySelector('category__btn.selected');
+  active.classList.remove()
 
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
